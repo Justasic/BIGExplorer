56114bc7b21f8b114412faa4a46b7d65f7bb34b8
Justin Crawford <Justasic@Gmail.com>
2015-10-30 16:19:26 -0700

    It compiles and shows something.

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,13 +9,18 @@
 
 // Include our interface.
 #include "ui_explorer.h"
+#include "mainwindow.h"
 
 std::vector<std::string> Arguments;
 
 int main(int argc, char **argv)
 {
 	Arguments = std::vector<std::string>(argv, argv+argc);
-
 	QApplication application(argc, argv);
+
+	MainWidget w;
+
+	w.show();
+	
 	return application.exec();
 }
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -12,10 +12,29 @@
 #include "ui_explorer.h"
 
 
-explicit MainWidget::MainWidget(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+MainWidget::MainWidget(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
 	ui->setupUi(this);
-	ui->statusbar->showMessage()
+	ui->statusbar->showMessage("Ready");
+
+	// We have 3 columns
+	ui->tableWidget->setColumnCount(3);
+	QStringList m_columnLabels;
+	m_columnLabels << "File" << "Type" << "Size";
+	// Set column names.
+	ui->tableWidget->setHorizontalHeaderLabels(m_columnLabels);
+	// Make column header visible (show column names)
+	ui->tableWidget->verticalHeader()->setVisible(false);
+	// Remove the grid-style since I want a list.
+	ui->tableWidget->setShowGrid(false);
+	// Don't allow editing of the cells.
+	ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+	// Select the entire row instead of a specific cell (since this is a list)
+	ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+	// Turn off horizontal scroll bar (it looks bad and most OTHER UIs I have seen remove them as well)
+	ui->tableWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+	// Stretch the horizontal cells to fit the text they contain.
+	ui->tableWidget->horizontalHeader()->setStretchLastSection(true);
 }
 
 MainWidget::~MainWidget()
@@ -24,8 +43,8 @@ MainWidget::~MainWidget()
 }
 
 // Qt slots
-void on_pushButton_play_clicked() {}
-void on_pushButton_pause_clicked() {}
-void on_pushButton_next_clicked() {}
-void on_pushButton_previous_clicked() {}
-void on_tableWidget_cellDoubleClicked(int, int) {}
+void MainWidget::on_pushButton_play_clicked() {}
+void MainWidget::on_pushButton_pause_clicked() {}
+void MainWidget::on_pushButton_next_clicked() {}
+void MainWidget::on_pushButton_previous_clicked() {}
+void MainWidget::on_tableWidget_cellDoubleClicked(int, int) {}
